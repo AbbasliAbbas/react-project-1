6635f4bb7f8c2c2c35144beb0bd9284d28285476
Your Name <elvireldar92@gmail.com>
2025-01-17 18:37:22 +0400

    my homework

--- vite-project/src/componets/Card.jsx
+++ vite-project/src/componets/Card.jsx
@@ -4,10 +4,10 @@ const Card = () => {
   return (
     <div className="  gap-[60px] mt-[50px] flex text-white">
     
-      <div className='bg-gray-800 rounded-[5px] w-[300px] h-[100px] pl-[20px] pt-[20px] ml-[180px]'>Card <br /><p className='text-gray-400 pt-[5px]'>This is my card.</p></div>
+      <div className='bg-gray-800 rounded-[5px] w-[300px] h-[100px] pl-[20px] pt-[20px] ml-[50px]'>Card <br /><p className='text-gray-400 pt-[5px]'>This is my card.</p></div>
       <div className='bg-gray-800 rounded-[5px] w-[300px] h-[100px] pl-[20px] pt-[20px]'>Card <br /><p className='text-gray-400 pt-[5px]'>This is my card.</p></div>
       <div className='bg-gray-800 rounded-[5px] w-[300px] h-[100px] pl-[20px] pt-[20px]'>Card <br /><p className='text-gray-400 pt-[5px]'>This is my card.</p></div>
-      
+      <div className='bg-gray-800 rounded-[5px] w-[300px] h-[100px] pl-[20px] pt-[20px] mr-[50px]'>Card <br /><p className='text-gray-400 pt-[5px]'>This is my card.</p></div>
     </div>
   )
 }
